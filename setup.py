615428a2bf028ed2cec3efaf9833c7dbbb8df731
maxirmx <m.samsonov@computer.org>
2021-04-07 16:53:48 +0300

    8

--- setup.py
+++ setup.py
@@ -1,16 +1,34 @@
-from setuptools import setup, find_packages
+import io
+import os
+import sys
+from setuptools import setup
 
-setup(
-    name = 'LSABE',
-    version = '0.0.1',
-    url = '',
-    description = 'Implementation of LSABE algorithm',
-    packages = find_packages(),
-    install_requires = [
-         "CharmCrypto>=0.50.0"
-    ],
-    dependency_links = [
-      "git+https://github.com/JHUISI/charm.git@master#egg=CharmCrypto-0.50.0",
+PACKAGES = ["lsabe"]
+
+def setup_module():
+
+  root = os.path.abspath(os.path.dirname(__file__))
+
+  with io.open(os.path.join(root, "lsabe", "__about__.py"), encoding="utf8") as f:
+            __about__ = {}
+            exec(f.read(), __about__)
+
+  with io.open(os.path.join(root, "README.md"), encoding="utf8") as f:
+    readme = f.read()
+
+  setup(
+    name=__about__["__name__"],
+    packages=PACKAGES,
+    description=__about__["__summary__"],
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author=__about__["__author__"],
+    author_email=__about__["__email__"],
+    version=__about__["__version__"],
+    url=__about__["__uri__"],
+    license=__about__["__license__"],
+    setup_requires = [
+      "CharmCrypto @ git+https://github.com/JHUISI/charm.git@master",
     ],
     classifiers=[
       "Environment :: Console",
@@ -21,6 +39,10 @@ setup(
       "Programming Language :: Python :: 3.7",
       "Topic :: Scientific/Engineering",
     ]
-)
+  )
+
+
+if __name__ == "__main__":
+    setup_module()
 
 # https://github.com/apache/beam/blob/master/sdks/python/apache_beam/examples/complete/juliaset/setup.py
