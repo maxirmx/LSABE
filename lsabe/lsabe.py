9e19d38e66d9396d91952fa3a6630ba634911d89
maxirmx <m.samsonov@computer.org>
2021-04-08 15:45:59 +0300

    LSABE 08-04

--- lsabe/lsabe.py
+++ lsabe/lsabe.py
@@ -1,5 +1,6 @@
 
 import os
+import re
 # https://jhuisi.github.io/charm/cryptographers.html
 from charm.toolbox.pairinggroup import PairingGroup,ZR,G1,G2,GT,pair
 
@@ -11,14 +12,50 @@ class LSABE():
 
     def SystemInit(self):
 # ....
-# Still some mistery here. 
-# g has to be "generator", but f just "belongs" to G
-        f, g = self.group.random(G1), self.group.random(G1)
+        f = self.group.random(G1) 
+        g = self.group.random(G1)
         alfa, beta, lmbda = self.group.random(ZR), self.group.random(ZR), self.group.random(ZR)        
         e_gg_alfa = pair(g, g) ** alfa
         self.MSK = { 'alfa':alfa, 'beta':beta, 'lambda':lmbda }        
         self.PP =  { 'f':f, 'g':g, 'g^beta':g ** beta, 'g^lambda':g ** lmbda, 'e(gg)^alfa':e_gg_alfa}
-        return (self.MSK, self.PP)
+
+        self.__serialize__(self.MSK, "/home/maxirmx/key.lsabe")
+        self.__serialize__(self.PP,  "/home/maxirmx/key.lsabe.pub")
+
+        self.__deserialize__MSK("/home/maxirmx/key.lsabe")
+        self.__deserialize__PP("/home/maxirmx/key.lsabe.pub")
+
+    def __serialize__(self, dict, filename):
+        file = open(filename, 'wb')
+        for v in dict.values():
+            file.write(self.group.serialize(v))
+        file.close
+
+    def __deserialize__MSK(self, filename):
+        file = open(filename, 'r')
+        data = file.read()
+        file.close
+        d = re.split('=', data)
+
+        self.MSK = {}
+        self.MSK['alfa']    = self.group.deserialize(d[0].encode())
+        self.MSK['beta']    = self.group.deserialize(d[1].encode())
+        self.MSK['lambda']  = self.group.deserialize(d[2].encode())
+
+    def __deserialize__PP(self, filename):
+        file = open(filename, 'r')
+        data = file.read()
+        file.close
+        d = re.split('=', data)
+
+        print (self.PP)
+        self.PP = {}
+        self.PP['f']            = self.group.deserialize(d[0].encode())
+        self.PP['g']            = self.group.deserialize(d[1].encode())
+        self.PP['g^beta']       = self.group.deserialize(d[2].encode())
+        self.PP['g^lambda']     = self.group.deserialize(d[3].encode())
+        self.PP['e(gg)^alfa']   = self.group.deserialize(d[4].encode())
+        print (self.PP)
 
     def KeyGen(self):
         t, delta = self.group.random(ZR), self.group.random(ZR)
@@ -33,15 +70,11 @@ class LSABE():
         TK5 = (self.PP['g'] ** (z * self.MSK['alfa']) ) * (self.PP['g'] ** (z * self.MSK['beta'] * t))   
         TK = (TK3, TK5)
         return (SK,TK)
-
 #
 def main():
     print('Hello, World!')
     lsabe = LSABE()
-    (MSK, PP) = lsabe.SystemInit()
-
+    lsabe.SystemInit()
     (SK, TK) = lsabe.KeyGen()
     
-    print(TK)
-
-
+ 
--- setup.py
+++ setup.py
@@ -1,9 +1,93 @@
+
+
 import io
 import os
 import sys
-from setuptools import setup
+import subprocess
+from setuptools import setup, find_packages
+from distutils.command.build import build as _build
 
-PACKAGES = ["lsabe"]
+# https://github.com/apache/beam/blob/master/sdks/python/apache_beam/examples/complete/juliaset/setup.py
+# This class handles the pip install mechanism.
+class build(_build):  # pylint: disable=invalid-name
+  """A build command class that will be invoked during package install.
+  The package built using the current setup.py will be staged and later
+  installed in the worker using `pip install package'. This class will be
+  instantiated during install for this specific scenario and will trigger
+  running the custom commands specified.
+  """
+  sub_commands = _build.sub_commands + [('CustomCommands', None)]
+
+
+# Some custom command to run during setup. The command is not essential for this
+# workflow. It is used here as an example. Each command will spawn a child
+# process. Typically, these commands will include steps to install non-Python
+# packages. For instance, to install a C++-based library libjpeg62 the following
+# two commands will have to be added:
+#
+#     ['apt-get', 'update'],
+#     ['apt-get', '--assume-yes', 'install', 'libjpeg62'],
+#
+# First, note that there is no need to use the sudo command because the setup
+# script runs with appropriate access.
+# Second, if apt-get tool is used then the first command needs to be 'apt-get
+# update' so the tool refreshes itself and initializes links to download
+# repositories.  Without this initial step the other apt-get install commands
+# will fail with package not found errors. Note also --assume-yes option which
+# shortcuts the interactive confirmation.
+#
+# Note that in this example custom commands will run after installing required
+# packages. If you have a PyPI package that depends on one of the custom
+# commands, move installation of the dependent package to the list of custom
+# commands, e.g.:
+#
+#     ['pip', 'install', 'my_package'],
+#
+
+CUSTOM_COMMANDS = [ 
+# PBC
+#                    ['rm', '-rf', 'pbc-0.5.14'],
+#                    ['wget', 'https://crypto.stanford.edu/pbc/files/pbc-0.5.14.tar.gz'],
+#                    ['tar', '-xvf', 'pbc-0.5.14.tar.gz'],
+#                    ['sh', '-c', 'cd pbc-0.5.14 && ./configure'],
+#                    ['sh', '-c', 'cd pbc-0.5.14 && make'],
+#                    ['sh', '-c', 'cd pbc-0.5.14 && make install'],
+#                    ['rm', '-rf', 'pbc-0.5.14'],
+# Charm crypto                     
+                    ['rm', '-rf', 'charm'],
+                    ['git', 'clone', 'https://github.com/JHUISI/charm.git'],
+                    ['sh', '-c', 'cd charm && ./configure.sh'],
+                    ['sh', '-c', 'cd charm && make'],
+                    ['sh', '-c', 'cd charm && make install'],
+                    ['rm', '-rf', 'charm']
+                  ]
+
+class CustomCommands(setuptools.Command):
+  """A setuptools Command class able to run arbitrary commands."""
+  def initialize_options(self):
+    pass
+
+  def finalize_options(self):
+    pass
+
+  def RunCustomCommand(self, command_list):
+    print('Running command: %s' % command_list)
+    p = subprocess.Popen(
+        command_list,
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT)
+    # Can use communicate(input='y\n'.encode()) if the command run requires
+    # some confirmation.
+    stdout_data, _ = p.communicate()
+    print('Command output: %s' % stdout_data)
+    if p.returncode != 0:
+      raise RuntimeError(
+          'Command %s failed: exit code: %s' % (command_list, p.returncode))
+
+  def run(self):
+    for command in CUSTOM_COMMANDS:
+      self.RunCustomCommand(command)
 
 def setup_module():
 
@@ -16,33 +100,36 @@ def setup_module():
   with io.open(os.path.join(root, "README.md"), encoding="utf8") as f:
     readme = f.read()
 
+  PACKAGES = find_packages()
+
   setup(
-    name=__about__["__name__"],
-    packages=PACKAGES,
-    description=__about__["__summary__"],
-    long_description=readme,
-    long_description_content_type="text/markdown",
-    author=__about__["__author__"],
-    author_email=__about__["__email__"],
-    version=__about__["__version__"],
-    url=__about__["__uri__"],
-    license=__about__["__license__"],
-    setup_requires = [
-      "CharmCrypto @ git+https://github.com/JHUISI/charm.git@master",
-    ],
+    name                          = __about__["__name__"],
+    packages                      = PACKAGES,
+    description                   =  __about__["__summary__"],
+    long_description              = readme,
+    long_description_content_type = "text/markdown",
+    author                        = __about__["__author__"],
+    author_email                  = __about__["__email__"],
+    version                       = __about__["__version__"],
+    url                           = __about__["__uri__"],
+    license                       = __about__["__license__"],
     classifiers=[
       "Environment :: Console",
       "Intended Audience :: Developers",
       "Intended Audience :: Science/Research",
-      "License :: OSI Approved :: MIT License",
       "Operating System :: POSIX :: Linux",
       "Programming Language :: Python :: 3.7",
-      "Topic :: Scientific/Engineering",
-    ]
+      "Programming Language :: Python :: 3.8",
+      "Topic :: Scientific/Engineering"
+    ],
+    cmdclass={
+        # Command class instantiated and run during pip install scenarios.
+        'build': build,
+        'CustomCommands': CustomCommands,
+    }
   )
 
 
 if __name__ == "__main__":
     setup_module()
 
-# https://github.com/apache/beam/blob/master/sdks/python/apache_beam/examples/complete/juliaset/setup.py
